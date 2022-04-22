f67f0bee35c9932297d27e16431d05ec4a507d8e
baileybrody <98329892+baileybrody@users.noreply.github.com>
2022-04-22 14:36:15 -0400

    Update

---
+++ hospitals.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 22 14:18:18 2022
+
+@author: brodybailey
+"""
+
+import pandas as pd 
+import geopandas as gpd 
+import numpy as np
+
+#importing data from Census prior to 2010 
+
+hospitals = pd.read_excel("new_york_hospital_data.xlsx")
+
+#removing closed and merged hospitals from the dataset
+
+hospitals = hospitals.
+                          
+
