e9be7fdf62f52d79b3cd31977331ac7e3945e802
baileybrody <98329892+baileybrody@users.noreply.github.com>
2022-05-11 15:30:45 -0400

    Update

--- hospitals.py
+++ hospitals.py
@@ -135,7 +135,7 @@ fg = sns.lmplot(data= utah_deaths, x="Miles", y="Average Deaths as % of Populati
 
 reg = scipy.stats.linregress(utah_deaths["Miles"],utah_deaths["Average Deaths as % of Population"])
 
-fig.savefig("regress.png")
+fg.savefig("regression.png")
 
 print("Intercept", reg.intercept)
 
